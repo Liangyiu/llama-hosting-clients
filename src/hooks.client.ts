ed6c6c4248c16a416c7c78275cd3742e3faf4f69
Liangyiu <ms290498@gmail.com>
2025-02-16 17:55:40 +0100

    found build error - sentry was the problem

--- src/hooks.client.ts
+++ src/hooks.client.ts
@@ -1,33 +1,37 @@
 import { dev } from '$app/environment';
 import { env } from '$env/dynamic/public';
-import { handleErrorWithSentry, replayIntegration } from '@sentry/sveltekit';
-import * as Sentry from '@sentry/sveltekit';
+// import { handleErrorWithSentry, replayIntegration } from '@sentry/sveltekit';
+// import * as Sentry from '@sentry/sveltekit';
 import type { HandleClientError } from '@sveltejs/kit';
 
-Sentry.init({
-	environment: dev ? 'development' : 'production',
-	dsn: env.PUBLIC_GLITCHTOP_DSN,
-	tracesSampleRate: 1.0,
+// Sentry.init({
+// 	environment: dev ? 'development' : 'production',
+// 	dsn: env.PUBLIC_GLITCHTOP_DSN,
+// 	tracesSampleRate: 1.0,
 
-	// This sets the sample rate to be 10%. You may want this to be 100% while
-	// in development and sample at a lower rate in production
-	replaysSessionSampleRate: 0.1,
+// 	// This sets the sample rate to be 10%. You may want this to be 100% while
+// 	// in development and sample at a lower rate in production
+// 	replaysSessionSampleRate: 0.1,
 
-	// If the entire session is not sampled, use the below sample rate to sample
-	// sessions when an error occurs.
-	replaysOnErrorSampleRate: 1.0,
+// 	// If the entire session is not sampled, use the below sample rate to sample
+// 	// sessions when an error occurs.
+// 	replaysOnErrorSampleRate: 1.0,
 
-	// If you don't want to use Session Replay, just remove the line below:
-	integrations: [replayIntegration()]
-});
+// 	// If you don't want to use Session Replay, just remove the line below:
+// 	integrations: [replayIntegration()]
+// });
 
 const errorId = crypto.randomUUID();
-Sentry.setTag('custom_error_id', errorId);
+// Sentry.setTag('custom_error_id', errorId);
 
-// If you have a custom error handler, pass it to `handleErrorWithSentry`
-export const handleError: HandleClientError = handleErrorWithSentry(() => {
+export const handleError: HandleClientError = ({ error, event }) => {
+	console.error('Client side error:', error, event);
+
 	return {
 		message: "An unexpected error occurred. We're working on it!",
 		errorId
 	};
-});
+};
+
+// If you have a custom error handler, pass it to `handleErrorWithSentry`
+// export const handleError: HandleClientError = handleErrorWithSentry(customErrorHandler);
--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,14 @@
-import { sentrySvelteKit } from '@sentry/sveltekit';
-import { purgeCss } from 'vite-plugin-tailwind-purgecss';
+// import { sentrySvelteKit } from '@sentry/sveltekit';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import Icons from 'unplugin-icons/vite';
 
 export default defineConfig({
 	plugins: [
-		sentrySvelteKit({
-			sourceMapsUploadOptions: {
-				org: 'llama-it',
-				project: 'llama-hosting-clients',
-				url: 'https://glitch.coolify.llama.onl'
-			}
-		}),
+		// sentrySvelteKit({
+		// 	adapter: 'node'
+		// }),
 		sveltekit(),
-		purgeCss(),
 		Icons({
 			compiler: 'svelte'
 		})
